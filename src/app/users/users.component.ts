4b54cbd14c57abe3df60cc0e33adbc9e9d89d89d
Filip M <filip.moszczynski@onet.pl>
2020-10-09 20:55:14 +0200

    usersList created

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { userInfo } from 'os';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-users',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+
+  usersList: Array<User> = [
+    {id:1, name:"Jan", phone:123456789, email:"jan@lol"},
+    {id:2, name:"Jacek", phone:234567891, email:"jacek@lol"},
+    {id:3, name:"Janina", phone:345678912, email:"janina@lol"},
+    {id:4, name:"Janush", phone:456789123, email:"janush@lol"},
+    {id:5, name:"Adrian", phone:567891234, email:"adrjan@lol"},
+    {id:6, name:"Bonawentura", phone:678912345, email:"bona@lol"},
+    {id:8, name:"Natalia", phone:891234567, email:"natalia@lol"},
+    {id:7, name:"Tomasz", phone:789123456, email:"tomasz@lol"},
+  ]
 
   constructor() { }
 
